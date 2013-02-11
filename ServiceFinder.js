dba3391c24c8cf65d1ad04ed0eb8c565a195f860
Nick Robinson <nick.robinson@pernod-ricard.com>
2013-02-11 14:51:31 -0500

    function to add initial data if data on server is empty

--- ServiceFinder.js
+++ ServiceFinder.js
@@ -15,5 +15,8 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
     Services = new Meteor.Collection("test-services");
+    if (Services.find().count() === 0) {
+      Services.insert({name: "Initial data", description: "This is test data"});
+    };
   });
 }
